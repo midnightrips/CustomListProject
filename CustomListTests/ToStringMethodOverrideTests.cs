17861af6ca448bb17e3c727981fae8c9bdd1f43a
Ehretz <sg3584@alum.barnard.edu>
2024-01-17 15:55:02 -0500

    Creating Test Class For ToString Method Override

---
+++ CustomListTests/ToStringMethodOverrideTests.cs
@@ -0,0 +1,47 @@
+﻿using CustomList;
+
+namespace CustomListTests
+{
+    [TestClass]
+    internal class ToStringMethodOverrideTests
+    {
+        [TestMethod] //test 1: a list of strings returns expected result
+        public void ToStringMethodOverride_CreateListOfString_ExpectedResultReturned() //method being tested_the situation we are testing_what we expect to happen
+        {
+            //arrange
+
+
+            //act
+
+
+            //assert
+
+        }
+
+        [TestMethod] //test 2: a list of ints returns an expected result
+        public void ToStringMethodOverride_CreateListOfInts_ExpectedResultReturned() //method being tested_the situation we are testing_what we expect to happen
+        {
+            //arrange
+
+
+            //act
+
+
+            //assert
+
+        }
+
+        [TestMethod] //test 3: an empty list returns an expty string
+        public void ToStringMethodOverride_CreateEmptyList_EmptyStringReturned() //method being tested_the situation we are testing_what we expect to happen
+        {
+            //arrange
+
+
+            //act
+
+
+            //assert
+
+        }
+    }
+}
